996c05ad973f25166f5af573432ea1ecbcea60a0
Petter <betterux@outlook.com>
2026-01-01 11:15:31 +0100

    feat: add docker compose setup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+//import 'dotenv/config'; - Not working in the container
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
